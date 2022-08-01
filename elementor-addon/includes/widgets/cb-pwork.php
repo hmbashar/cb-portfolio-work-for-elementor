c3ad39b20b7813851c7186761df0ba511ae0c03b
hmbashar <hmbashar@gmail.com>
2022-08-02 04:20:15 +0600

    posts per page dynamic for elementor

--- elementor-addon/includes/widgets/cb-pwork.php
+++ elementor-addon/includes/widgets/cb-pwork.php
@@ -27,19 +27,20 @@ class Elementor_CB_Pwork extends \Elementor\Widget_Base {
         // Content Tab Start
 
         $this->start_controls_section(
-            'section_title',
+            'cb_pwork_content_section',
             [
-                'label' => esc_html__( 'Title', 'elementor-addon' ),
+                'label' => esc_html__( 'Settings', 'cbpw' ),
                 'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
             ]
         );
 
         $this->add_control(
-            'title',
+            'cb_pwork_count',
             [
-                'label' => esc_html__( 'Title', 'elementor-addon' ),
-                'type' => \Elementor\Controls_Manager::TEXTAREA,
-                'default' => esc_html__( 'Hello world', 'elementor-addon' ),
+                'label' => esc_html__( 'How Many posts show?', 'cbpw' ),
+                'type' => \Elementor\Controls_Manager::NUMBER,
+                'default' => esc_html__( 8, 'cbpw' ),
+                'label_block' => true,
             ]
         );
 
@@ -48,12 +49,36 @@ class Elementor_CB_Pwork extends \Elementor\Widget_Base {
         // Content Tab End
 
 
+            // Content Tab Start
+
+            $this->start_controls_section(
+                'section_title2',
+                [
+                    'label' => esc_html__( 'Title 2', 'cbpw' ),
+                    'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+                ]
+            );
+    
+            $this->add_control(
+                'title2',
+                [
+                    'label' => esc_html__( 'Title 2', 'cbpw' ),
+                    'type' => \Elementor\Controls_Manager::TEXT,
+                    'default' => esc_html__( 'Hello world 2', 'cbpw' ),
+                ]
+            );
+    
+            $this->end_controls_section();
+    
+            // Content Tab End
+
+
         // Style Tab Start
 
         $this->start_controls_section(
             'section_title_style',
             [
-                'label' => esc_html__( 'Title', 'elementor-addon' ),
+                'label' => esc_html__( 'Title', 'cbpw' ),
                 'tab' => \Elementor\Controls_Manager::TAB_STYLE,
             ]
         );
@@ -61,7 +86,7 @@ class Elementor_CB_Pwork extends \Elementor\Widget_Base {
         $this->add_control(
             'title_color',
             [
-                'label' => esc_html__( 'Text Color', 'elementor-addon' ),
+                'label' => esc_html__( 'Text Color', 'cbpw' ),
                 'type' => \Elementor\Controls_Manager::COLOR,
                 'selectors' => [
                     '{{WRAPPER}} .hello-world' => 'color: {{VALUE}};',
@@ -79,9 +104,60 @@ class Elementor_CB_Pwork extends \Elementor\Widget_Base {
         $settings = $this->get_settings_for_display();
         ?>
 
-        <p class="hello-world">
-            <?php echo $settings['title']; ?>
-        </p>
+            <div class="cb_pwork-our-works-area">
+                
+                <?php 
+
+                    $post_count = $settings['cb_pwork_count']; // how many posts do you want to show?
+                    $our_works = new \WP_Query(array(
+                        'post_type'	=> 'our-works', 
+                        'posts_per_page' => $post_count,
+                    ));
+                                            
+                if($our_works->have_posts()) : while($our_works->have_posts()) : $our_works->the_post();
+                    $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); // get thumbnail full size
+            
+            
+                ?>
+                <!-- Single Our Work -->	
+                
+                <div class="cb_pwork-our-works">
+                    <div class="cb_pwork-our-work-thumb">
+                        <?php the_post_thumbnail('our-work');
+                            the_excerpt();
+                        ?>
+                    </div>
+                    <div class="cb_pwork-our-work-content">
+                        <div class="cb_pwork-our-work-icons">
+                            <a href="<?php the_permalink();?>"><i class="fas fa-link"></i></a>
+                            <a href="<?php echo esc_url($featured_img_url);?>"><i class="fas fa-search"></i></a>
+                        </div>
+                        <div class="cb_pwork-our-work-title">
+                            <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+                            <p>
+                                <?php 
+                                $cb_pwork_work_cats = get_the_terms(get_the_ID(), 'work_category');								
+
+                                    if(is_array($cb_pwork_work_cats)) {
+                                    foreach ($cb_pwork_work_cats as $cb_pwork_cat) {
+                                        $work_cat_slug = get_term_link($cb_pwork_cat->slug, 'work_category');								 
+                                        echo '<a href="'.esc_url($work_cat_slug).'">'.esc_html($cb_pwork_cat->name).'</a>';
+                                    }
+                                    }
+                                ?>
+                                
+                            </p>
+                        </div>
+                        
+                        
+                    </div>
+                </div>		
+                <!--/single our work-->
+                <?php endwhile; endif; ?>
+                
+                
+            </div>
+	
 
         <?php
     }
